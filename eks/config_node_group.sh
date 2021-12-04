8ad8b1a5f89d2ffc2d1e4dec1427997b06865d8b
Mitchell Cunningham <mitchac@gmail.com>
2021-12-04 12:47:50 +1000

    add script for nodegroup setup in EKS

---
+++ eks/config_node_group.sh
@@ -0,0 +1,8 @@
+echo $1
+echo $2
+STACK_NAME=$(eksctl get nodegroup --region us-east-1 --cluster $1 -o json | jq -r --arg nodegroup $2 '.[]|select(.Name==$nodegroup) | .StackName')
+echo $STACK_NAME
+ROLE_NAME=$(aws cloudformation describe-stack-resources --region us-east-1 --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
+aws iam put-role-policy --role-name $ROLE_NAME --policy-name S3-Policy-For-Worker --policy-document file://k8s-s3-policy.json
+aws iam get-role-policy --role-name $ROLE_NAME --policy-name S3-Policy-For-Worker
+
