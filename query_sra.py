b28228c413c80d3da3a431977d1fd532f4e61778
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-02-08 11:19:47 +1000

    query_sra: Query for metadata too.

--- query_sra.py
+++ query_sra.py
@@ -36,29 +36,7 @@ import extern
 
 sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')] + sys.path
 
-if __name__ == '__main__':
-    parent_parser = argparse.ArgumentParser()
-
-    parent_parser.add_argument('--date',required=True, help='date of run e.g. 20111130')
-    
-    parent_parser.add_argument('--debug', help='output debug information', action="store_true")
-    #parent_parser.add_argument('--version', help='output version information and quit',  action='version', version=repeatm.__version__)
-    parent_parser.add_argument('--quiet', help='only output errors', action="store_true")
-
-    args = parent_parser.parse_args()
-
-    # Setup logging
-    if args.debug:
-        loglevel = logging.DEBUG
-    elif args.quiet:
-        loglevel = logging.ERROR
-    else:
-        loglevel = logging.INFO
-    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
-    raise Exception("Need to add some exceptions e.g. SRR1563167 (libraryselection=unspecified)")
-
-    sql = """
+BUILD_TAXONOMY_TABLE_SQL = """
 -- STEP 1: Create temporary table with metagenome taxonomy entries
 -- Loop counter
 DECLARE counter int64 default 1;
@@ -102,20 +80,9 @@ END WHILE;
 -- -- SELECT  xlevel, tax_id, parent_id, sci_name  FROM test.emp ORDER BY xlevel;
 
 -- STEP 2: Define output
+"""
 
--- Now do the full query 
-EXPORT DATA OPTIONS(
-  uri='__SQL_JSON_RESULTS_URI__',
-  format='JSON') AS
-
-SELECT
--- count(*)
-  acc,
-  mbases,
-  mbytes
-FROM
-  `nih-sra-datastore.sra.metadata`
-WHERE
+SQL_WHERE_CLAUSE = """
   acc IN (
   SELECT
     acc
@@ -137,13 +104,89 @@ WHERE
 ;
 """
 
+if __name__ == '__main__':
+    parent_parser = argparse.ArgumentParser()
+
+    parent_parser.add_argument('--date',required=True, help='date of run e.g. 20111130')
+    parent_parser.add_argument('--where-clause', help='where clause to use [default all metagenomes]')
+    
+    parent_parser.add_argument('--debug', help='output debug information', action="store_true")
+    #parent_parser.add_argument('--version', help='output version information and quit',  action='version', version=repeatm.__version__)
+    parent_parser.add_argument('--quiet', help='only output errors', action="store_true")
+
+    args = parent_parser.parse_args()
+
+    # Setup logging
+    if args.debug:
+        loglevel = logging.DEBUG
+    elif args.quiet:
+        loglevel = logging.ERROR
+    else:
+        loglevel = logging.INFO
+    logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    logging.warning("Need to add some exceptions e.g. SRR1563167 (libraryselection=unspecified)")
+
+    if args.where_clause:
+        where_clause = args.where_clause
+    else:
+        where_clause = SQL_WHERE_CLAUSE
+
+    sql = BUILD_TAXONOMY_TABLE_SQL + """
+-- Now do the full query 
+EXPORT DATA OPTIONS(
+  uri='__SQL_JSON_RESULTS_URI__',
+  format='JSON') AS
+
+SELECT
+  acc,
+  assay_type,
+  center_name,
+  experiment,
+  sample_name,
+  instrument,
+  libraryselection,
+  librarysource,
+  platform,
+  sample_acc,
+  biosample,
+  organism,
+  sra_study,
+  releasedate,
+  bioproject,
+  avgspotlen,
+  mbases,
+  insertsize,
+  library_name,
+  biosamplemodel_sam,
+  collection_date_sam,
+  geo_loc_name_country_calc,
+  geo_loc_name_country_continent_calc,
+  geo_loc_name_sam,
+  sample_name_sam,
+  attributes
+FROM
+  `nih-sra-datastore.sra.metadata`
+WHERE
+""" + where_clause
+
     # https://www.ncbi.nlm.nih.gov/sra/?term=SRR12280810 - covid, source VIRAL RNA - want to exclude
     # https://www.ncbi.nlm.nih.gov/sra?term=srr7694367 covid, source VIRAL RNA - want to exclude
 
-    destination_path = 'gs://bowerbird-bigquery-testing/sra_{}/*'.format(args.date)
+    destination_path = 'gs://bowerbird-bigquery-testing/sra_metadata_{}/*'.format(args.date)
     logging.info("Querying and writing results to {} ..".format(destination_path))
 
     sql = sql.replace('__SQL_JSON_RESULTS_URI__',destination_path)
 
     extern.run('bq query --use_legacy_sql=false', stdin=sql)
-    logging.info("Finished querying")
+    logging.info("Finished querying for metadata")
+
+
+    # Now write the taxonomy table
+    destination_path = 'gs://bowerbird-bigquery-testing/sra_taxonomy_table_{}/*'.format(args.date)
+    logging.info("Querying and writing taxonomy table results to {} ..".format(destination_path))
+
+    sql = "EXPORT DATA OPTIONS(uri='{}', format='JSON') AS select * from test.emp".format(destination_path)
+
+    extern.run('bq query --use_legacy_sql=false', stdin=sql)
+    logging.info("Finished querying for the taxonomy table")
